7519affed8af09597bc7fd719fee765ef678a8a4
Geelbuik3421 <60668064+Geelbuik3421@users.noreply.github.com>
2023-03-20 22:30:55 +0100

    APIcorrectie

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x4402C62616Fb1ebAB3D19662985E39D3b88d1526";
+const WHITELIST_CONTRACT_ADDRESS = "0x9e593a065E8e9D70f805f29c4423C69f25A94f0B";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-wheat-gamma.vercel.app/";
+const METADATA_URL = "https://nft-collection-wheat-gamma.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
